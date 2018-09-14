77943c77b872207871ce6e0210ab053e394cc341
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-09-14 13:39:32 -0400

    test: modified the Deck test cases

--- src/test/java/core/DeckTest.java
+++ src/test/java/core/DeckTest.java
@@ -1,31 +1,36 @@
 package core;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
 
-public class DeckTest {
-	Card aCard =new Card("ace",1);
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase{
+	Card aCard =new Card("ACE",1);
 	Deck aDeck =new Deck(52);
-	
-	
-	public void testPushAndPop() {
-		aDeck.push(aCard);
-		assertEquals(aCard, aDeck.pop());
+	Card temp;
+	public void testPushPop() {
+		aDeck.push(aCard);	
+		assertEquals(false, aDeck.isEmpty());
+		temp=aDeck.pop();
+		assertEquals(aCard.getRank(), temp.getRank());
+		assertEquals(aCard.getSuit(), temp.getSuit());
+		
 	}
 	public void testIsEmpty() {
-		assertEquals(-1, aDeck.isEmpty());
+		assertEquals(true, aDeck.isEmpty());
 	}
 	public void testShuffleDeck() {
 		aDeck.createDeck();
 		aDeck.pop();
 		aDeck.push(aCard);
-		assertNotEquals(aCard, aDeck.shuffleDeck());
+		aDeck.shuffleDeck();
+		temp=aDeck.pop();
+		assertNotEquals(aCard.getRank(), temp.getRank());
+		assertNotEquals(aCard.getSuit(), temp.getSuit());
 	}
 	public void testCreateDeck() {
 		aDeck.createDeck();
 		assertNotNull(aDeck.pop());
-		assertEquals(aCard, aDeck.pop());
 	}
 
 
