cf34b6773b27850230c789789848f2a473ec2879
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-09-17 16:19:30 -0400

    Hand: created Hand Class

---
+++ src/main/java/core/Hand.java
@@ -0,0 +1,87 @@
+package core;
+
+public class Hand {
+	    private Card first;
+	    private int cardTotal;
+	    private String name;
+	    private int handSize;
+
+	    public Hand(String name)
+	    {
+	        first = null;
+	        this.name = name;
+	        cardTotal = 0;
+	        handSize = 0;
+	    } 
+
+	    public void insert(Card card)
+	    {
+	        Card newLink = new Card(card);
+	        newLink.next = first;
+
+	        if (card.getRank() == 1 && cardTotal + card.getValue() > 21)
+	            cardTotal = cardTotal + (card.getValue() - 10);
+	        else
+	            cardTotal = cardTotal + card.getValue();
+
+	        handSize = handSize + 1;
+
+	        first = newLink;
+	    } 
+
+	    public Card deleteFirst()
+	    {
+	        Card temp = first;
+	        first = first.next;
+	        cardTotal = cardTotal - temp.getValue();
+	        handSize = handSize - 1;
+	        return temp;
+	    } 
+
+	    public void displayHand()
+	    {
+	        Card current = first;
+	        while(current != null)
+	        {
+	            current.toString();
+	            current = current.next;
+	        } 
+	    } 
+
+	    public boolean isEmpty()
+	    {
+	        return first == null;
+	    } 
+
+	    public boolean bothEqual()
+	    {
+	        Card temp = first;
+	        return temp != null && (temp.getValue() == temp.next.getValue());
+	    } 
+
+	    public void peek()
+	    {
+	        first.toString();
+	    } 
+
+	    public int peekValue()
+	    {
+	        return first.getValue();
+	    } 
+
+	    public int getHandSize()
+	    {
+	        return handSize;
+	    } 
+
+	    public String getName()
+	    {
+	        return name;
+	    } 
+
+	    public int getHandTotal()
+	    {
+	        return cardTotal;
+	    } 
+	
+}
--- src/test/java/core/HandTest.java
+++ src/test/java/core/HandTest.java
@@ -5,37 +5,43 @@ import junit.framework.TestCase;
 public class HandTest extends TestCase{
 	Card aCard =new Card("DIAMOND",1);
 	Hand P1= new Hand("Bamise");
-	P1.insert(aCard);
-
+	
 	public void testInsert() {
+		P1.insert(aCard);
+		Card temp=P1.deleteFirst();
 		assertEquals(aCard.toString(),temp.toString());
 	}
 	public void testDeleteFirst() {
+		P1.insert(aCard);
 		Card temp=P1.deleteFirst();
 		assertEquals(aCard.toString(),temp.toString());
 	}
 	public void testIsEmpty() {
-		P1.deleteFirst();
-		assertNull(P1.isEmpty());
+		assertTrue(P1.isEmpty());
 	}
 	public void testBothEqual() {
 		P1.insert(aCard);
+		P1.insert(aCard);
 		assertTrue(P1.bothEqual());
-		Card temp=new Card("SPADE",13)
+		Card temp=new Card("SPADE",13);
 		P1.deleteFirst();
 		P1.insert(temp);
-		assertFalse(P1.bothEqual(););
+		assertFalse(P1.bothEqual());
 	}
 	public void testPeekValue() {
+		P1.insert(aCard);
 		assertEquals(11, P1.peekValue());
 	}
 	public void testGetHandSize() {
+		P1.insert(aCard);
 		assertEquals(1, P1.getHandSize());
 	}
 	public void testGetName() {
+		P1.insert(aCard);
 		assertEquals("Bamise", P1.getName());
 	}
 	public void testGetHandTotal() {
-		assertEquals(11, P1.getCardTotal());
+		P1.insert(aCard);
+		assertEquals(11, P1.getHandTotal());
 	}
 }
